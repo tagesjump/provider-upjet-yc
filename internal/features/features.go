b87197c424fafb81285cef2c895fbce5fa03026b
Sergey Scheglov <50084893+ssch1337@users.noreply.github.com>
2023-10-01 20:35:26 +0500

    Initial commit

---
+++ internal/features/features.go
@@ -0,0 +1,20 @@
+/*
+ Copyright 2022 Upbound Inc
+*/
+
+package features
+
+import "github.com/crossplane/crossplane-runtime/pkg/feature"
+
+// Feature flags.
+const (
+	// EnableAlphaExternalSecretStores enables alpha support for
+	// External Secret Stores. See the below design for more details.
+	// https://github.com/crossplane/crossplane/blob/390ddd/design/design-doc-external-secret-stores.md
+	EnableAlphaExternalSecretStores feature.Flag = "EnableAlphaExternalSecretStores"
+
+	// EnableAlphaManagementPolicies enables alpha support for
+	// Management Policies. See the below design for more details.
+	// https://github.com/crossplane/crossplane/pull/3531
+	EnableAlphaManagementPolicies feature.Flag = "EnableAlphaManagementPolicies"
+)
